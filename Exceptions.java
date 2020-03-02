80b5639a48083e133cbd1eaadc4f5131cf34ab82
ranjith909 <ranjithrao@suntechnoservices.com>
2020-03-02 19:43:27 +0530

    Linked List

---
+++ Exceptions.java
@@ -0,0 +1,18 @@
+package com.exception;
+
+public class Exceptions {
+
+	public static void main(String[] args) {
+		try
+		{
+			int a=2/0;
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+
+	}
+
+}
---
+++ Linkedlist.java
@@ -0,0 +1,15 @@
+package com.exception;
+import java.util.*;
+
+public class Linkedlist {
+
+	public static void main(String[] args) {
+		LinkedList<String> li=new LinkedList<String>();
+		li.add("madhu");
+		li.add("ranjith");
+		System.out.println(li);
+		
+
+	}
+
+}
